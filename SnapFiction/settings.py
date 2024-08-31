9575635b95d5f7b1ad75120841c2f6f4d1ff7132
iammadhanraj <madhanreigns312@gmail.com>
2024-09-01 00:42:35 +0530

    small_update

--- SnapFiction/settings.py
+++ SnapFiction/settings.py
@@ -118,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Directory where static files will be collected (for production use)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional directories to look for static files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
